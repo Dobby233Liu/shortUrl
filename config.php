a104074ca1f7bb8e73b3c2af076bf3be7ff83f14
lwys <papaqi1234567@163.com>
2019-07-23 21:47:29 +0800

    oh

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 /**
  * @Author: Ding Jianlong
  * @Date:   2019-03-20 22:44:12
- * @Last Modified by:   Ding Jianlong
- * @Last Modified time: 2019-03-24 23:06:22
+ * @Last Modified by:   lwys
+ * @Last Modified time: 404 not found
  */
 
 
@@ -12,14 +12,10 @@
 
 
 $config = array(
-	'title' => "短网址演示",                     //网站标题
-	'site' => "https://51015.cn/demo/shortUrl",  //短网址域名
+	'site' => "https://114514.cn",  //短网址域名
 	//不允许缩短的域名，单个匹配，*表示所有的二级域名
-	'blackList' => array('*.51015.cn','baidu1.com','youku1.com'),
-	'key' => "idjl",                             //token 使用的密钥
+	'key' => "145170dif",                             //token 使用的密钥
 
 	//根据需求修改
-	'use_rewrite' => 1,                          // 是否使用伪静态,默认使用
-	//生成的短网址类型：abc表示字母数字混合，123为纯数字累加方式
-	'type' => 'abc',
+	'use_rewrite' => 1
 );
--- create.php
+++ create.php
@@ -10,19 +10,34 @@
 
 error_reporting(E_ALL ^ E_NOTICE);//显示除去 E_NOTICE 之外的所有错误信息
 require "config.php";
-require "function.php";
-
-if ($config['type'] == 'abc') {
-    $saveFile = 'urlsabc.php';
-} elseif ($config['type'] == '123') {
-    $saveFile = 'urls123.php';
-} else {
-    exit('config.php中设置错误，请检查。');
+//生成大小写字母和数字随机字符串
+function createId($len){
+    //大小写字母和数字混用
+    $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $key = '';
+    for($i = 0;$i < $len;$i++){
+        $key .= $str{mt_rand(0,strlen($str)-1)};
+    }
+    return $key;
 }
 
+//查看随机生成的id 是否已经被占用,占用重新生成，递归
+function checkId($arr,$id){
+    if(!isset($arr[$id])){
+        return $id;
+    }else{
+        $id = createId(6);
+        return checkId($arr,$id);
+    }
+}
+
+
+$saveFile = 'urls.php';
+
+$keyshit = trim($_POST["sig"]);
 // 检查本地文件是否可读写
 if (!is_writable($saveFile) || !is_readable($saveFile)) {
-    die('请将urlsabc.php（或urls123.php）文件设置为777权限');
+    die('chmod 777 urls.php');
 }
 
 //引入保存网址的php文件
@@ -31,61 +46,49 @@ $arr = require $saveFile;
 // 判断是生成还是跳转
 @$id = trim($_GET['id']);
 if ($id) {
+    $message = "";
     //跳转
     if ($arr[$id]) {
-        header('location:' . $arr[$id]);
+        header('Location:' . $arr[$id]);
     }
-    $message = "不存在此短网址" . $config['site'] .'/'. $id;
-    echo json_encode(array('code' => 8404, 'message' => $message));
+    $message = "not found " . $id;
+    echo $message;
     exit();
 } else {
     //生成
-    $token = $_POST['token'];
     $url = trim($_POST['url']);
-    if (!checkToken($config['key'],$token)) {
-        $message = 'tooken错误，请访问'.$config['site'];
-        echo json_encode(array('code' => 8000, 'message' => $message));
-        exit();
-    }
     if ($url == '') {
-        $message = '没有输入URL地址';
-        echo json_encode(array('code' => 8001, 'message' => $message));
+        $message = 'nourl';
+        echo $message;
         exit();
     }
     //判断是否正则为正确的网址
     $regex = "/^(https?:\/\/)([\w-]+\.)+[\w-]+(\/[\w-.\/?%&=#]*)?$/i";
     if (!preg_match($regex, $url)) {
-        $message = '请输入正确的URL地址';
-        echo json_encode(array('code' => 8002, 'message' => $message));
+        $message = 'bad url';
+        echo $message;
         exit();
     }
     //判断是否是黑名单域名
-    if (!checkBlackList($url, $config['blackList'])) {
-        $message = '抱歉，暂时无法缩短该域名';
-        echo json_encode(array('code' => 8003, 'message' => $message));
-        exit();
-    }
+    
     //检查是否已存在重复网址
     $find = array_search($url, $arr);
     if ($find !== false) {
         $id = $find;   //返回之前的短网址
     } else {
         //原来没有新插入
-        if ($config['type'] == 'abc') {
-            $id = createId(5);    //随机生成5位小数字母+数字
-            $id = checkId($arr, $id);
-            //原来没有新插入
-            $arr[$id] = $url;
-        } elseif ($config['type'] == '123') {
-            $arr[] = $url;
-            $id = count($arr) - 1;
-
-        }
+        $id = createId(6);    //随机生成5位小数字母+数字
+        if(isset($keyshit) && $keyshit != '') $id=$keyshit;
+        $id = checkId($arr, $id);
+        //原来没有新插入
+        $arr[$id] = $url;
         $a = '<?php' . PHP_EOL . 'return ' . var_export($arr, true) . ';';
         file_put_contents($saveFile, $a);
     }
-    $message = "缩短网址成功";
+    $message = "success<br>";
     $shortUrl = ($config['use_rewrite'] == 1) ? "{$config['site']}/{$id}" : "{$config['site']}/create.php?id={$id}";
-    echo json_encode(array('code' => 200, 'message' => $message, 'shortUrl' => $shortUrl));
+    echo $message;
+    echo "<img src='genqr.php?c=".$shortUrl."'><br>";
+    echo "url: <a href='".$shortUrl."'>".$shorturl."</a>";
     exit();
 }
